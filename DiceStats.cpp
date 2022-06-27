d955f0cb98990781ddcf36599879a621a760eea6
deanbunn <deanbunn@gmail.com>
2022-06-26 19:19:10 -0700

    20220626_1918hrs Initial Setup

---
+++ DiceStats.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int dice_count{ 20 };
+    int cup_count{ 40 };
+
+    std::cout << "The number of dice is " << dice_count << std::endl;
+    std::cout << "The number of cups are " << cup_count << std::endl;
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Dice Stats
+
+My personal training repo for learning both C++ and statistics 
+
+
