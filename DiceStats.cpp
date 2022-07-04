a5c970ecced19287fc5e0c8736d8bbc943e7888f
deanbunn <deanbunn@gmail.com>
2022-07-04 16:34:18 -0700

    20220704_1634hrs Learning about pointers

--- DiceStats.cpp
+++ DiceStats.cpp
@@ -8,4 +8,14 @@ int main()
     std::cout << "The number of dice is " << dice_count << std::endl;
     std::cout << "The number of cups are " << cup_count << std::endl;
 
+    std::cout << "The positive range for type double is from "
+        << std::numeric_limits<double>::min() << " to "
+        << std::numeric_limits<double>::max() << std::endl;
+
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << "Currently, variable i is " << i << std::endl;
+    }
+
 }
--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 My personal training repo for learning both C++ and statistics 
 
+***
+
+#### Learning Log
+
+- 2022-07-04: reading about pointers 
 
